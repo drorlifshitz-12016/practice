15842e2f1accab68562c02be4da8751b8728a6c5
lahavnave <lahavnave8@gmail.com>
2023-03-19 20:18:22 +0200

    servo practice

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/itay/practice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/itay/practice.java
@@ -1,5 +1,4 @@
 package org.firstinspires.ftc.teamcode.itay;
-
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -25,16 +24,24 @@ public class practice extends LinearOpMode {
         backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
 
         while (opModeIsActive()){
-                frontRight.setPower(gamepad1.right_trigger - gamepad1.left_trigger);
-                frontLeft.setPower(gamepad1.right_trigger -  gamepad1.left_trigger);
-                backRight.setPower(gamepad1.right_trigger - gamepad1.left_trigger);
-                backLeft.setPower(gamepad1.right_trigger - gamepad1.left_trigger);
+                double Powermotor_FR = gamepad1.left_stick_y + gamepad1.left_stick_x - gamepad1.right_stick_x;
+                double Powermotor_FL = gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x;
+                double Powermotor_BR = gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x;
+                double Powermotor_BL = gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x;
 
-
-                double Powermotor_FR = gamepad1.left_stick_y + gamepad1.left_stick_x
-                double Powermotor_FL = gamepad1.left_stick_y - gamepad1.left_stick_x
-                double Powermotor_BR = gamepad1.left_stick_y - gamepad1.left_stick_x
-                double Powermotor_BL =gamepad1.left_stick_y + gamepad1.left_stick_x 
-       }
+                double highestNum_B = Math.max(Math.abs(Powermotor_BL),Math.abs(Powermotor_BR));
+                double highestNum_F = Math.max(Math.abs(Powermotor_FR),Math.abs(Powermotor_FL));
+                double highestNum = Math.max(highestNum_B,highestNum_F);
+                if (highestNum > 1){
+                    Powermotor_BL = Powermotor_BL / highestNum;
+                    Powermotor_BR = Powermotor_BR / highestNum;
+                    Powermotor_FL = Powermotor_FL / highestNum;
+                    Powermotor_FR = Powermotor_FR / highestNum;
+                }
+            frontRight.setPower(Powermotor_FR);
+            frontLeft.setPower(Powermotor_FL);
+            backRight.setPower(Powermotor_BR);
+            backLeft.setPower(Powermotor_BL);
+        }
    }
 }
