3abbd31aa2297fd15c6be1d82934c895cfbb1582
mishmash <mishmashbgu@gmail.com>
2023-04-16 20:13:17 +0300

    rotate

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ido/rotation.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.ido;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+public class rotation extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        DcMotor frontRight = hardwareMap.dcMotor.get("frontRight");
+        DcMotor frontLeft  = hardwareMap.dcMotor.get("frontLeft" );
+        DcMotor backRight  = hardwareMap.dcMotor.get("backRight" );
+        DcMotor backLeft   = hardwareMap.dcMotor.get("backLeft"  );
+
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft .setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+        if(isStopRequested()){
+            return;
+        }
+        resetRuntime();
+
+        while (opModeIsActive()){
+            frontRight.setPower(gamepad1.right_stick_x);
+            frontLeft .setPower(-gamepad1.right_stick_x);
+            backRight .setPower(gamepad1.right_stick_x);
+            backLeft  .setPower(gamepad1.right_stick_x);
+        }
+
+        frontRight.setPower(0);
+        frontLeft.setPower(0);
+        backRight.setPower(0);
+        backLeft.setPower(0);
+        
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ido/straight.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.ido;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+public class straight extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        DcMotor frontRight = hardwareMap.dcMotor.get("frontRight");
+        DcMotor frontLeft  = hardwareMap.dcMotor.get("frontLeft" );
+        DcMotor backRight  = hardwareMap.dcMotor.get("backRight" );
+        DcMotor backLeft   = hardwareMap.dcMotor.get("backLeft"  );
+
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft .setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+        if(isStopRequested()){
+            return;
+        }
+        resetRuntime();
+
+        while (opModeIsActive()){
+                frontRight.setPower(gamepad1.left_stick_y);
+                frontLeft .setPower(gamepad1.left_stick_y);
+                backRight .setPower(gamepad1.left_stick_y);
+                backLeft  .setPower(gamepad1.left_stick_y);
+        }
+
+        frontRight.setPower(0);
+        frontLeft.setPower(0);
+        backRight.setPower(0);
+        backLeft.setPower(0);
+
+    }
+}
