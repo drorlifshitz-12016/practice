01cc6ebb43b7292243576d7ede2a3fc324cd2252
mishmash12016 <mishmashbgu@gmail.com>
2023-04-16 20:12:14 +0300

    intake arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/itay/ExtendArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/itay/ExtendArm.java
@@ -7,32 +7,39 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp
 public class ExtendArm extends LinearOpMode {
 
+    // region POSITION CONSTANTS
+    // the positions of the servo for both the in and out arm positions
     static final double IN_POSITION = 0.55;
     static final double OUT_POSITION = 0.04;
+    // endregion
 
+    // region SERVO VARIABLES
     static Servo armRight;
     static Servo armLeft;
+    // endregion
 
     @Override
     public void runOpMode() throws InterruptedException {
+        // region SERVO INITIALIZATION
+        // get servos from the hardware map
         armRight = hardwareMap.servo.get("armRight");
         armLeft  = hardwareMap.servo.get("armLeft" );
 
+        // set the right servo to be reverse
         armRight.setDirection(Servo.Direction.REVERSE);
 
+        // set the servos to be in
         extendArm(0);
+        // endregion
 
+        // region AWAIT START
         waitForStart();
-        if (isStopRequested()) {
-            return;
-        }
-
+        if (isStopRequested()) return;
         resetRuntime();
+        // endregion
 
         while (opModeIsActive()) {
-
             extendArm(gamepad1.right_trigger);
-
         }
     }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/itay/practice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/itay/practice.java
@@ -4,24 +4,63 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class practice extends LinearOpMode {
+
+    static Servo grabberRight;
+    static Servo grabberLeft ;
+
     @Override
     public void runOpMode() throws InterruptedException {
 
+        grabberRight = hardwareMap.servo.get("grabberRight");
+        grabberLeft  = hardwareMap.servo.get("grabberLeft" );
+
+        grabberRight.setDirection(Servo.Direction.REVERSE);
+
+        double inPosition = 0.7;
+        double highLevel1 = 0.055;
+        double highLevel2 = 0.06;
+        double highLevel3 = 0.09;
+        double highLevel4 = 0.13;
+        double highLevel5 = 0.17;
+
+        grabberLeft.setPosition(inPosition);
+        grabberRight.setPosition(inPosition);
 
         waitForStart();
         if(isStopRequested()){
             return;
         }
 
+
         resetRuntime();
-
-
-
         while (opModeIsActive()){
 
+            if(gamepad1.left_trigger > 0.08) {
+                grabberRight.setPosition(highLevel1);
+                grabberLeft.setPosition(highLevel1);
+
+            }else if(gamepad1.dpad_up) {
+                grabberRight.setPosition(highLevel5);
+                grabberLeft.setPosition(highLevel5);
+
+            }else if(gamepad1.dpad_right) {
+                grabberRight.setPosition(highLevel4);
+                grabberLeft.setPosition(highLevel4);
+
+            }else if(gamepad1.dpad_down) {
+                grabberRight.setPosition(highLevel3);
+                grabberLeft.setPosition(highLevel3);
+
+            }else if(gamepad1.dpad_left){
+                grabberRight.setPosition(highLevel2);
+                grabberLeft.setPosition(highLevel2);
+
+            }
         }
-   }
+    }
 }
+
