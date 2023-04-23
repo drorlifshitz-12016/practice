eb5d78cd0c799e31d3d37e4bfcc0d9a2ae2ad96e
itay12016 <128303898+itay12016@users.noreply.github.com>
2023-04-23 20:25:05 +0300

    temp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/itay/Imu.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.itay;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class Imu extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        waitForStart();
+        if(isStopRequested()){ return; }
+        resetRuntime();
+
+        while (opModeIsActive()){
+            
+        }
+    }
+}
+
