6e2ffd328058c2113a1dbf15ee90f44f109b029d
itay12016 <128303898+itay12016@users.noreply.github.com>
2023-04-25 19:44:04 +0300

    highst of arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/itay/practice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/itay/practice.java
@@ -16,55 +16,73 @@ public class practice extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
 
         grabberRight = hardwareMap.servo.get("grabberRight");
-        grabberLeft  = hardwareMap.servo.get("grabberLeft" );
+        grabberLeft = hardwareMap.servo.get("grabberLeft");
 
         grabberRight.setDirection(Servo.Direction.REVERSE);
 
-        double[] highets = {0.7,0.055,0.06,0.09,0.13,0.17};
+        double[] highets = {0.7, 0.055, 0.06, 0.09, 0.13, 0.17};
 
         grabberRight.setPosition(highets[0]);
-        grabberLeft .setPosition(highets[0]);
+        grabberLeft.setPosition(highets[0]);
+
+        boolean last_dpad;
+        boolean dpad = false;
+        boolean dpad_click;
+        boolean second_click = false;
+        boolean is_high_0 = true;
+        boolean triger_left = false;
+        boolean last_triger_left;
 
         waitForStart();
-        if(isStopRequested()){
+        if (isStopRequested()) {
             return;
         }
 
         resetRuntime();
-        while (opModeIsActive()){
+        while (opModeIsActive()) {
 
-            if(gamepad1.left_trigger > 0.08) {
+            last_triger_left = triger_left;
+            triger_left = (gamepad1.left_trigger > 0.1);
+
+            last_dpad = dpad;
+            dpad = gamepad1.dpad_up || gamepad1.dpad_right || gamepad1.dpad_down || gamepad1.dpad_left;
+            dpad_click = dpad && !last_dpad;
+
+
+            if (triger_left) {
                 grabberRight.setPosition(highets[1]);
-                grabberLeft .setPosition(highets[1]);
-
-            }else if(grabberRight.getPosition() == highets[0]) {
-
+                grabberLeft.setPosition(highets[1]);
+            } else if (last_triger_left && !triger_left) {
+                grabberRight.setPosition(highets[0]);
+                grabberLeft. setPosition(highets[0]);
+            }
+            if (dpad_click && is_high_0) {
                 if (gamepad1.dpad_up) {
                     grabberRight.setPosition(highets[5]);
-                    grabberLeft.setPosition(highets[5]);
+                    grabberLeft. setPosition(highets[5]);
 
-                } else if (gamepad1.dpad_right) {
+
+                }else if (gamepad1.dpad_right) {
                     grabberRight.setPosition(highets[4]);
-                    grabberLeft.setPosition(highets[4]);
+                    grabberLeft. setPosition(highets[4]);
 
-                } else if (gamepad1.dpad_down) {
+                }else if (gamepad1.dpad_down) {
                     grabberRight.setPosition(highets[3]);
-                    grabberLeft.setPosition(highets[3]);
+                    grabberLeft. setPosition(highets[3]);
 
-                } else if (gamepad1.dpad_left) {
+
+                }else if (gamepad1.dpad_down) {
                     grabberRight.setPosition(highets[2]);
                     grabberLeft.setPosition(highets[2]);
-
-                } else if (grabberLeft.getPosition() != highets[0] && (gamepad1.dpad_up || gamepad1.dpad_right || gamepad1.dpad_down || gamepad1.dpad_left)) {
-                    grabberLeft.setPosition(highets[0]);
-                    grabberRight.setPosition(highets[0]);
                 }
-                if (gamepad1.dpad_up && grabberLeft.getPosition() == highets[5]) {
-                    grabberLeft.setPosition(highets[0]);
-                    grabberRight.setPosition(highets[0]);
+
+            if (dpad_click && !is_high_0){
+                grabberLeft.setPosition(highets[0]);
+                grabberRight.setPosition(highets[0]);
+                }
+
+
                 }
             }
         }
     }
-}
-
