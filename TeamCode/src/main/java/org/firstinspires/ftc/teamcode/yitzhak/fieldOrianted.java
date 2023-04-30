14c5f78bb7e4e6a1b358f84fe27ad3ea6d4670a5
mishmash12016 <mishmashbgu@gmail.com>
2023-04-30 16:45:45 +0300

    working field orianted

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/yitzhak/fieldOrianted.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/yitzhak/fieldOrianted.java
@@ -1,10 +1,15 @@
 package org.firstinspires.ftc.teamcode.yitzhak;
 
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 
 @TeleOp
 public class fieldOrianted extends LinearOpMode {
@@ -14,51 +19,52 @@ public class fieldOrianted extends LinearOpMode {
         DcMotor frontLeft  = hardwareMap.dcMotor.get("frontLeft" );
         DcMotor backRight  = hardwareMap.dcMotor.get("backRight" );
         DcMotor backLeft   = hardwareMap.dcMotor.get("backLeft"  );
+        BNO055IMU imu      = hardwareMap.get(BNO055IMU.class, "imu");
 
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
+        imu.initialize(parameters);
 
         frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeft .setDirection(DcMotorSimple.Direction.REVERSE);
 
-
         waitForStart();
-        if(isStopRequested()){
-            return;
-        }
+        if(isStopRequested()){ return; }
         resetRuntime();
-        double L
-        double t;
-        double x1;
-        double y1;
-        double x2;
-        double y2;
-        double a;
-        double b = 0;
-
+        
+        double L, t, x1, y1, x2, y2, a, b;
 
         while (opModeIsActive()){
+            // region UPDATING THE INPUT
+            b  = -imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.RADIANS).firstAngle;
             y1 = -gamepad1.left_stick_y;
             x1 =  gamepad1.left_stick_x;
-            t =  gamepad1.right_stick_x;
+            t  =  gamepad1.right_stick_x;
+            // endregion
+
+            // region DOING THE CALCULATION
             L = Math.sqrt(x1 * x1 + y1 * y1);
             a = Math.atan2(y1, x1);
-           y2 = L * (Math.sin(a + b));
-           x2 = L * (Math.cos(a + b));
+            y2 = L * Math.sin(a + b);
+            x2 = L * Math.cos(a + b);
+            // endregion
 
             // region SECOND PART
-            double fR = (y-x-t);
-            double fL = (y+x+t);
-            double bR = (y+x-t);
-            double bL = (y-x+t);
+            double fR = y2 - x2 - t;
+            double fL = y2 + x2 + t;
+            double bR = y2 + x2 - t;
+            double bL = y2 - x2 + t;
 
-            double max1 = Math.max(fR , fL);
-            double max2 = Math.max(bR , bL);
+            double max1 = Math.max(Math.abs(fR) , Math.abs(fL));
+            double max2 = Math.max(Math.abs(bR) , Math.abs(bL));
             double maxF = Math.max(max2 , max1);
+            if(maxF>1) {
 
-            fR /= maxF;
-            fL /= maxF;
-            bR /= maxF;
-            bL /= maxF;
-
+                fR /= maxF;
+                fL /= maxF;
+                bR /= maxF;
+                bL /= maxF;
+            }
             frontRight.setPower(fR);
             frontLeft .setPower(fL);
             backRight .setPower(bR);
