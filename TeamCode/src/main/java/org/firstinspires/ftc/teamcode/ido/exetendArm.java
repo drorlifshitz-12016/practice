4a7b30ad05f0d4eb265c3d412d93bb47db237e93
Diabeto72 <idoamir100@gmail.com>
2023-05-21 18:09:51 +0300

    extendArm

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ido/exetendArm.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.ido;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp
+public class exetendArm extends LinearOpMode {
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        Servo grabberRight = hardwareMap.servo.get("grabberRight");
+        Servo grabberLeft  = hardwareMap.servo.get("grabberLeft" );
+
+
+
+
+        waitForStart();
+        if(isStopRequested()){
+            return;
+        }
+        resetRuntime();
+
+        while (opModeIsActive()){
+            grabberRight.setPosition(0.3);
+            grabberLeft.setPosition(0.3);
+        }
+
+
+
+    }
+}
