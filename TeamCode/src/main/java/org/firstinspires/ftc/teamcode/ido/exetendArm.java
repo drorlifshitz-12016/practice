389e6295574b35c578545f39911c4bd29431ed77
Diabeto72 <idoamir100@gmail.com>
2023-06-11 20:26:21 +0300

    Update exetendArm.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ido/exetendArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ido/exetendArm.java
@@ -3,9 +3,6 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class exetendArm extends LinearOpMode {
@@ -17,6 +14,9 @@ public class exetendArm extends LinearOpMode {
     static Servo grabber;
     static Servo armRight;
     static Servo armLeft;
+
+    static final double IN_POSITION = 0.55;
+    static final double OUT_POSITION = 0.04;
 
     static double grabPosition = 0.40;
     static double openPosition = 0.18;
@@ -33,7 +33,7 @@ public class exetendArm extends LinearOpMode {
 
 
 
-        public static void resetServos(){
+    public static void resetServos(){
 
         grabber.setPosition(openPosition);
         grabberLeft.setPosition(0.055);
@@ -72,13 +72,17 @@ public class exetendArm extends LinearOpMode {
         resetServos();
         while (opModeIsActive()){
 
+            armLeft.setPosition(gamepad1.left_trigger * (OUT_POSITION - IN_POSITION) + IN_POSITION);
+            armRight.setPosition(gamepad1.left_trigger * (OUT_POSITION - IN_POSITION) + IN_POSITION);
+           if (gamepad1.left_trigger > 0 ) {
 
-            if (gamepad1.left_trigger > 0 ) {
-                armLeft.setPosition(gamepad1.left_trigger);
-                armRight.setPosition(gamepad1.left_trigger);
                 grabberRight.setPosition(0.85);
                 grabberLeft.setPosition(0.85);
-            }
+            } else {
+
+               grabberRight.setPosition(0.055);
+               grabberLeft.setPosition(0.055);
+           }
 
             if (gamepad1.right_trigger > 0) {
                 grabber.setPosition(grabPosition);
