ba4e6d53be83ca086f128ac1b8e36c6bd92196ae
Diabeto72 <idoamir100@gmail.com>
2023-06-15 19:14:49 +0300

    Create elevator.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ido/elevator.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.ido;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+public class elevator {
+}
