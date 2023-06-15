3e3215a4ee79eaceaed2e827ff1fe8630afcad7c
Diabeto72 <idoamir100@gmail.com>
2023-06-15 19:15:33 +0300

    Update elevator.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ido/elevator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ido/elevator.java
@@ -1,9 +1,38 @@
 package org.firstinspires.ftc.teamcode.ido;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.hardware.Servo;
-
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-public class elevator {
+@TeleOp
+public class elevator extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        DcMotor motorRight = (DcMotorEx) hardwareMap.dcMotor.get("elevatorRight");
+        DcMotor motorLeft = (DcMotorEx) hardwareMap.dcMotor.get("elevatorLeft");
+        DcMotor motorMiddle = (DcMotorEx) hardwareMap.dcMotor.get("elevatorMiddle");
+
+        motorRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        motorLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        waitForStart();
+        if (isStopRequested()) {return;}
+        resetRuntime();
+
+
+        while (opModeIsActive()){
+
+            motorLeft.setPower(gamepad1.left_trigger);
+            motorRight.setPower(gamepad1.left_trigger);
+            motorMiddle.setPower(gamepad1.left_trigger);
+
+            telemetry.addData("tick:", motorLeft.getCurrentPosition());
+            telemetry.update();
+
+        }
+    }
 }
