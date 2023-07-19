b749e6dc3e9e22893ea98438447df8de83d5e157
lahavnave <lahavnave8@gmail.com>
2023-07-19 07:36:44 +0300

    setup testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/swerve/driving/moduleTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/swerve/driving/moduleTesting.java
@@ -3,48 +3,26 @@ package org.firstinspires.ftc.teamcode.swerve.driving;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
-@Disabled
+import java.util.Base64;
+
 @TeleOp
 public class moduleTesting extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
+        DcMotor motor = hardwareMap.dcMotor.get("motor");
+        CRServo servo = hardwareMap.get(CRServo.class, "servo");
 
-        // p = power
-
-        // 12   : ORR (overall reduction ratio)
-        // 6000 : MMS (motor max speed)
-
-        // 55.556 = 111.111 / 2 : servoRPM
-        // 111.111 : initial servo rpm
-        // 2       : SPR (servo pulley ratio)
-
-        // WWS (wanted wheel speed) : inputSpeed * WMS
-        // 495 : WMS (wheel max speed)
-        // inputSpeed : 0 -> 1
-
-        // WWS = (MMS * p - servoRPM) / ORR
-        // WWS * ORR = MMS * p - servoRPM
-        // WWS * ORR + servoRPM = MMS * p
-        // (WWS * ORR + servoRPM) / MMS = p
-
-        // p = (inputSpeed * WMS * ORR + servoRPM) / MMS
 
         waitForStart();
         if(isStopRequested()) { return; }
         resetRuntime();
 
-    }
-    // region CONSTANTS
-    final static int motorReductionRatio = 12;
-    final static int maxMotorSpeed = 6000;
-    final static double initialServoSpeed = 111.111;
-    final static double servoReductionRatio = 2;
-    final static double servoRPM = initialServoSpeed / servoReductionRatio;
-
-    final static double maxWheelSpeed = (maxMotorSpeed - servoRPM) / motorReductionRatio;
-    // endregion
-    static double calculateMotorSpeed(double power){
-        return (power * maxWheelSpeed * motorReductionRatio + servoRPM) / maxMotorSpeed;
+        while (opModeIsActive()){
+            servo.setPower(1);
+        }
     }
 }
