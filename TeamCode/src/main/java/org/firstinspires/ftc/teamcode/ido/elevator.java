cc4c4c22a66468b82c693f71f101088628b79c82
Diabeto72 <idoamir100@gmail.com>
2023-07-27 17:24:07 +0300

    Update elevator.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ido/elevator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ido/elevator.java
@@ -14,7 +14,7 @@ public class elevator extends LinearOpMode {
         DcMotor motorLeft = (DcMotorEx) hardwareMap.dcMotor.get("elevatorLeft");
         DcMotor motorMiddle = (DcMotorEx) hardwareMap.dcMotor.get("elevatorMiddle");
         final  int middlePosition = 11300;
-        final int lowPosition    = 1 ;
+        final int lowPosition    = 4300 ;
         final int highPosition   = 17500;
 
         motorRight.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -22,20 +22,27 @@ public class elevator extends LinearOpMode {
         motorLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motorLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
+
         waitForStart();
         telemetry.addData("tick:", motorLeft.getCurrentPosition());
 
         while (opModeIsActive()){
 
+
+
             /*motorLeft.setPower(gamepad1.left_trigger);
             motorRight.setPower(gamepad1.left_trigger);
             motorMiddle.setPower(gamepad1.left_trigger);*/
 
-        if(gamepad1.a == true){
-            motorLeft.setPower(lowPosition);
-            motorRight.setPower(lowPosition);
-            motorMiddle.setPower(lowPosition);
-           
+        if(gamepad1.a){
+            motorLeft.setPower(0.65);
+            motorRight.setPower(0.65);
+            motorMiddle.setPower(0.65);
+
+            if (motorRight.getCurrentPosition() == lowPosition && motorLeft.getCurrentPosition() == lowPosition && motorMiddle.getCurrentPosition() == lowPosition)
+                motorLeft.setPower(0.23);
+                motorRight.setPower(0.23);
+                motorMiddle.setPower(0.23);
             }
 
             telemetry.addData("leftmotor", motorLeft.getCurrentPosition());
