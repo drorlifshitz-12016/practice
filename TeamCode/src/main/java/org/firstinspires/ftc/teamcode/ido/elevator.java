c22c05108d292d8cad1d4550d1308267f03e0994
Diabeto72 <idoamir100@gmail.com>
2023-07-27 17:37:16 +0300

    test1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ido/elevator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ido/elevator.java
@@ -13,9 +13,11 @@ public class elevator extends LinearOpMode {
         DcMotor motorRight = (DcMotorEx) hardwareMap.dcMotor.get("elevatorRight");
         DcMotor motorLeft = (DcMotorEx) hardwareMap.dcMotor.get("elevatorLeft");
         DcMotor motorMiddle = (DcMotorEx) hardwareMap.dcMotor.get("elevatorMiddle");
+
         final  int middlePosition = 11300;
         final int lowPosition    = 4300 ;
         final int highPosition   = 17500;
+
 
         motorRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
@@ -24,14 +26,14 @@ public class elevator extends LinearOpMode {
 
 
         waitForStart();
-        telemetry.addData("tick:", motorLeft.getCurrentPosition());
-
+        telemetry.addData(":", motorLeft.getCurrentPosition());
         while (opModeIsActive()){
 
 
 
             /*motorLeft.setPower(gamepad1.left_trigger);
             motorRight.setPower(gamepad1.left_trigger);
+
             motorMiddle.setPower(gamepad1.left_trigger);*/
 
         if(gamepad1.a){
@@ -48,10 +50,6 @@ public class elevator extends LinearOpMode {
 
             telemetry.addData("leftmotor", motorLeft.getCurrentPosition());
         }
-
-
-
-
 
 
         }
