14032a1a679d55ba972406ab06da9eaadeaa3d86
lahavnave <lahavnave8@gmail.com>
2023-08-03 20:52:19 +0300

    started configuring the system

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/swerve/driving/moduleTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/swerve/driving/moduleTesting.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.swerve.driving;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -11,18 +12,45 @@ import java.util.Base64;
 
 @TeleOp
 public class moduleTesting extends LinearOpMode {
+    static AnalogInput encoder;
+
     @Override
     public void runOpMode() throws InterruptedException {
         DcMotor motor = hardwareMap.dcMotor.get("motor");
         CRServo servo = hardwareMap.get(CRServo.class, "servo");
-
+        encoder = hardwareMap.get(AnalogInput.class, "encoder");
 
         waitForStart();
         if(isStopRequested()) { return; }
         resetRuntime();
 
         while (opModeIsActive()){
-            servo.setPower(1);
+            servo.setPower(calcPower2(getEncoderAngle(), gamepad1.left_stick_x ));
+            telemetry.addData("angle", getEncoderAngle());
+            telemetry.update();
         }
+    }
+    public static double getEncoderAngle(){
+        return encoder.getVoltage() / encoder.getMaxVoltage();
+    }
+
+    public static double calcPower(double angle, double wantedAngle){
+        if (Math.abs(angle - wantedAngle) < 0.003){
+            return 0;
+        } else if (angle > wantedAngle){
+            return angle - wantedAngle + 0.05;
+        } else {
+            return angle - wantedAngle - 0.05;
+        }
+
+    }
+    final static double stoppingDst = 0.08;
+    public static double calcPower2(double angle, double wantedAngle){
+
+        if (Math.abs(angle - wantedAngle) > stoppingDst){
+            return (angle - wantedAngle) > 0 ? 1: -1;
+        }
+
+        return (Math.cos((angle - wantedAngle) / stoppingDst * Math.PI) / 2 - 0.5) * (angle - wantedAngle > 0 ? -1: 1) / 3;
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing.java
@@ -3,16 +3,39 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.CRServo;
 
-@Disabled
+//@Disabled
 @TeleOp
 public class testing extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
+        CRServo servoFR = hardwareMap.get(CRServo.class, "frontRight");
+        CRServo servoFL = hardwareMap.get(CRServo.class, "frontRight");
+        CRServo servoBR = hardwareMap.get(CRServo.class, "frontRight");
+        CRServo servoBL = hardwareMap.get(CRServo.class, "frontRight");
+
+        servoFR.setPower(0.5);
+        //servoFL.setPower(0.5);
+        //servoBR.setPower(0.5);
+        //servoBL.setPower(0.5);
+
+        AnalogInput E0 = hardwareMap.get(AnalogInput.class, "0");
+        AnalogInput E1 = hardwareMap.get(AnalogInput.class, "1");
+        AnalogInput E2 = hardwareMap.get(AnalogInput.class, "2");
+        AnalogInput E3 = hardwareMap.get(AnalogInput.class, "3");
 
         waitForStart();
         if(isStopRequested()) { return; }
         resetRuntime();
 
+        while (opModeIsActive()){
+            telemetry.addData("E0", E0.getVoltage() / E0.getMaxVoltage());
+            telemetry.addData("E1", E1.getVoltage() / E1.getMaxVoltage());
+            telemetry.addData("E2", E2.getVoltage() / E2.getMaxVoltage());
+            telemetry.addData("E3", E3.getVoltage() / E3.getMaxVoltage());
+            telemetry.update();
+        }
     }
 }
