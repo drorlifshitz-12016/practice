e605fad0bfdf72e164f350e4fbd7d0813ea6c597
lahavnave <lahavnave8@gmail.com>
2023-08-07 14:45:18 +0300

    made a working module controlling pid ( without  considering the dc motors)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/swerve/driving/moduleTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/swerve/driving/moduleTesting.java
@@ -12,45 +12,64 @@ import java.util.Base64;
 
 @TeleOp
 public class moduleTesting extends LinearOpMode {
-    static AnalogInput encoder;
 
+    final static double minPower = 0.12;
+    final static double maxPower = 1  ;
+
+    final static double acceptableError  = 0.02;
+    final static double slowingDistance  = 0.1;
+    final static double deceleratingDist = 0.13;
     @Override
     public void runOpMode() throws InterruptedException {
-        DcMotor motor = hardwareMap.dcMotor.get("motor");
-        CRServo servo = hardwareMap.get(CRServo.class, "servo");
-        encoder = hardwareMap.get(AnalogInput.class, "encoder");
+        // region SERVOS
+        CRServo servoFR = hardwareMap.get(CRServo.class, "frontRight");
+        CRServo servoFL = hardwareMap.get(CRServo.class, "frontLeft");
+        CRServo servoBR = hardwareMap.get(CRServo.class, "backRight");
+        CRServo servoBL = hardwareMap.get(CRServo.class, "backLeft");
+        // endregion
+
+        // region ENCODERS
+        AnalogInput encoderFR = hardwareMap.get(AnalogInput.class, "frontRight");
+        AnalogInput encoderBR = hardwareMap.get(AnalogInput.class, "backRight");
+        AnalogInput encoderBL = hardwareMap.get(AnalogInput.class, "backLeft");
+        AnalogInput encoderFL = hardwareMap.get(AnalogInput.class, "frontLeft");
+        // endregion
+
+        //servoFR.setPower(0.5);
+        //servoFL.setPower(0.5);
+        //servoBR.setPower(0.5);
+        //servoBL.setPower(0.5);
 
         waitForStart();
         if(isStopRequested()) { return; }
         resetRuntime();
 
         while (opModeIsActive()){
-            servo.setPower(calcPower2(getEncoderAngle(), gamepad1.left_stick_x ));
-            telemetry.addData("angle", getEncoderAngle());
+            servoFL.setPower(calcPower(calcDist(gamepad1.left_trigger, encoderFL.getVoltage() / encoderFL.getMaxVoltage())));
+
+            telemetry.addData("encoderFR", encoderFR.getVoltage() / encoderFR.getMaxVoltage());
+            telemetry.addData("encoderBR", encoderBR.getVoltage() / encoderBR.getMaxVoltage());
+            telemetry.addData("encoderBL", encoderBL.getVoltage() / encoderBL.getMaxVoltage());
+            telemetry.addData("encoderFL", encoderFL.getVoltage() / encoderFL.getMaxVoltage());
             telemetry.update();
         }
     }
-    public static double getEncoderAngle(){
-        return encoder.getVoltage() / encoder.getMaxVoltage();
+    public static double calcDist(double targetPos, double currentPos){
+        return (targetPos - currentPos + 1.5) % 1 - 0.5;
+    }
+    public static double calcPower(double dist){
+        if (dist > 0) { return -moveTypeDefinition( dist); }
+        else          { return  moveTypeDefinition(-dist); }
+    }
+    public static double moveTypeDefinition(double x){
+        if      (x < acceptableError ) { return 0; }
+        else if (x < slowingDistance ) { return minPower; }
+        else if (x < deceleratingDist) { return decelerationTypeDefinition(x); }
+        else                           { return maxPower; }
+    }
+    public static double decelerationTypeDefinition(double x){
+        // linear deceleration
+        return (x - slowingDistance) * (maxPower - minPower) / (deceleratingDist - slowingDistance) + minPower;
     }
 
-    public static double calcPower(double angle, double wantedAngle){
-        if (Math.abs(angle - wantedAngle) < 0.003){
-            return 0;
-        } else if (angle > wantedAngle){
-            return angle - wantedAngle + 0.05;
-        } else {
-            return angle - wantedAngle - 0.05;
-        }
-
-    }
-    final static double stoppingDst = 0.08;
-    public static double calcPower2(double angle, double wantedAngle){
-
-        if (Math.abs(angle - wantedAngle) > stoppingDst){
-            return (angle - wantedAngle) > 0 ? 1: -1;
-        }
-
-        return (Math.cos((angle - wantedAngle) / stoppingDst * Math.PI) / 2 - 0.5) * (angle - wantedAngle > 0 ? -1: 1) / 3;
-    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.AnalogInput;
@@ -9,33 +8,65 @@ import com.qualcomm.robotcore.hardware.CRServo;
 //@Disabled
 @TeleOp
 public class testing extends LinearOpMode {
+
+    final static double minPower = 0.2;
+    final static double maxPower = 1  ;
+
+    final static double acceptableError  = 0.05;
+    final static double slowingDistance  = 0.2;
+    final static double deceleratingDist = 0.4;
     @Override
     public void runOpMode() throws InterruptedException {
+        // region SERVOS
         CRServo servoFR = hardwareMap.get(CRServo.class, "frontRight");
-        CRServo servoFL = hardwareMap.get(CRServo.class, "frontRight");
-        CRServo servoBR = hardwareMap.get(CRServo.class, "frontRight");
-        CRServo servoBL = hardwareMap.get(CRServo.class, "frontRight");
+        CRServo servoFL = hardwareMap.get(CRServo.class, "frontLeft");
+        CRServo servoBR = hardwareMap.get(CRServo.class, "backRight");
+        CRServo servoBL = hardwareMap.get(CRServo.class, "backLeft");
+        // endregion
 
-        servoFR.setPower(0.5);
-        //servoFL.setPower(0.5);
-        //servoBR.setPower(0.5);
-        //servoBL.setPower(0.5);
+        // region ENCODERS
+        AnalogInput encoderFR = hardwareMap.get(AnalogInput.class, "frontRight");
+        AnalogInput encoderFL = hardwareMap.get(AnalogInput.class, "frontLeft");
+        AnalogInput encoderBR = hardwareMap.get(AnalogInput.class, "backRight");
+        AnalogInput encoderBL = hardwareMap.get(AnalogInput.class, "backLeft");
+        // endregion
 
-        AnalogInput E0 = hardwareMap.get(AnalogInput.class, "0");
-        AnalogInput E1 = hardwareMap.get(AnalogInput.class, "1");
-        AnalogInput E2 = hardwareMap.get(AnalogInput.class, "2");
-        AnalogInput E3 = hardwareMap.get(AnalogInput.class, "3");
+        /*
 
+        servoFR.setPower();
+        servoFL.setPower();
+        servoBR.setPower();
+        servoBL.setPower();
+
+         */
         waitForStart();
         if(isStopRequested()) { return; }
         resetRuntime();
 
         while (opModeIsActive()){
-            telemetry.addData("E0", E0.getVoltage() / E0.getMaxVoltage());
-            telemetry.addData("E1", E1.getVoltage() / E1.getMaxVoltage());
-            telemetry.addData("E2", E2.getVoltage() / E2.getMaxVoltage());
-            telemetry.addData("E3", E3.getVoltage() / E3.getMaxVoltage());
+            telemetry.addData("encoderFR", encoderFR.getVoltage() / encoderFR.getMaxVoltage());
+            telemetry.addData("encoderBR", encoderBR.getVoltage() / encoderBR.getMaxVoltage());
+            telemetry.addData("encoderBL", encoderBL.getVoltage() / encoderBL.getMaxVoltage());
+            telemetry.addData("encoderFL", encoderFL.getVoltage() / encoderFL.getMaxVoltage());
             telemetry.update();
         }
     }
+    public static double calcDist(double targetPos, double currentPos){
+        return (targetPos - currentPos + 1.5) % 1 - 0.5;
+    }
+    public static double calcPower(double dist){
+        if (dist > 0) { return  moveTypeDefinition( dist); }
+        else          { return -moveTypeDefinition(-dist); }
+    }
+    public static double moveTypeDefinition(double x){
+        if      (x < acceptableError ) { return 0; }
+        else if (x < slowingDistance ) { return minPower; }
+        else if (x < deceleratingDist) { return decelerationTypeDefinition(x); }
+        else                           { return maxPower; }
+    }
+    public static double decelerationTypeDefinition(double x){
+        // linear deceleration
+        return (x - slowingDistance) * (maxPower - minPower) / (deceleratingDist - slowingDistance) + minPower;
+    }
+
 }
