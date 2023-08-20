d320ab09e59ae8dcdd8d4b65852d9a11f38097a4
Tamirperezt <141968090+Tamirperezt@users.noreply.github.com>
2023-08-20 20:11:26 +0300

    turn dc

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing.java
@@ -2,18 +2,32 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
 public class testing extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
+    DcMotor frontright = hardwareMap.dcMotor.get("frontRight");
+    DcMotor frontleft = hardwareMap.dcMotor.get("frontleft");
+    DcMotor backright = hardwareMap.dcMotor.get("backright");
+    DcMotor backleft = hardwareMap.dcMotor.get("backlrft");
+
+
         waitForStart();
-        if (isStopRequested()) { return; }
-        resetRuntime();
+    if (isStopRequested()){ return; }
+    resetRuntime();
 
-        while (opModeIsActive()){
+    while (opModeIsActive()){
+        frontleft.setPower(-1);
+        frontright.setPower(1);
+        backleft.setPower(-1);
+        backright.setPower(1);
+        
+    }
 
-        }
+
+
     }
 }
