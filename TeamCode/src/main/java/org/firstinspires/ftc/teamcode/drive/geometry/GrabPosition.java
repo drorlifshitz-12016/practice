a11f902651d53baf9ce8859dfe94c229d0a05464
itay12016 <128303898+itay12016@users.noreply.github.com>
2023-08-22 19:21:42 +0300

    z

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/geometry/GrabPosition.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.drive.geometry;
+
+public class GrabPosition {
+    public long clawDelay, turretDelay;
+    public double fourbarPos, pivotPos;
+    public double   intakeTargetPosition;
+
+    public GrabPosition(int pos, long clawDelay, double fourbarPos, double pivotPos, long upDelay) {
+        this.clawDelay = clawDelay;
+        this.turretDelay = upDelay;
+        this.pivotPos = pivotPos;
+        this.fourbarPos = fourbarPos;
+        this.intakeTargetPosition = pos;
+    }
+}
