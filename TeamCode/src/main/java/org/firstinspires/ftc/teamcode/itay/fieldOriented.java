d45685eb5d218925a5d4eea149add511d5645dc7
itay12016 <128303898+itay12016@users.noreply.github.com>
2023-08-31 19:54:48 +0300

    add Road Runner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/itay/fieldOriented.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/itay/fieldOriented.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode.itay;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
-import com.qualcomm.hardware.broadcom.BroadcomColorSensor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -20,9 +19,9 @@ public class fieldOriented extends LinearOpMode {
 
         // region GET DRIVETRAIN MOTORS
         DcMotor frontRight = hardwareMap.dcMotor.get("frontRight");
-        DcMotor frontLeft  = hardwareMap.dcMotor.get("frontLeft" );
-        DcMotor backRight  = hardwareMap.dcMotor.get("backRight" );
-        DcMotor backLeft   = hardwareMap.dcMotor.get("backLeft"  );
+        DcMotor frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        DcMotor backRight = hardwareMap.dcMotor.get("backRight");
+        DcMotor backLeft = hardwareMap.dcMotor.get("backLeft");
         // endregion
         BNO055IMU imu = hardwareMap.get(BNO055IMU.class, "imu");
 
@@ -33,7 +32,7 @@ public class fieldOriented extends LinearOpMode {
         // region SET MOTOR DIRECTION
         // reversing the right motors in order to have intuition for their movement direction
         frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRight .setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
         // endregion
 
         Vector v1 = new Vector(0, 0);
@@ -43,53 +42,57 @@ public class fieldOriented extends LinearOpMode {
         double alpha;
 
         waitForStart();
-        if (isStopRequested()) { return; }
+        if (isStopRequested()) {
+            return;
+        }
         resetRuntime();
 
 
-        while (opModeIsActive()){
+        while (opModeIsActive()) {
 
 
+            //v1.setByCardinal(gamepad1.left_stick_x, -gamepad1.left_stick_y);
+            v1.set(gamepad1.left_stick_x, -gamepad1.left_stick_y);
 
-            v1.setByCardinal(gamepad1.left_stick_x, -gamepad1.left_stick_y);
-
-            alpha = v1.angle();
+            //alpha = v1.angle();
+            alpha = v1.getAngle();
             beta = -imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.RADIANS).firstAngle;
 
-            v2.setByAngular(v1.length(), alpha + beta);
+            //v2.setByAngular(v1.length(), alpha + beta);
+            v2.setByAngle(v1.getLength(), alpha + beta);
 
-                // region MOTOR POWER CALCULATION
-                // calculating the motor powers based on the three basic movements (straight, lateral and turn)
+            // region MOTOR POWER CALCULATION
+            // calculating the motor powers based on the three basic movements (straight, lateral and turn)
 
-                //                      [        straight       ] [         lateral       ] [         turn         ]
-                double frontRightPower =          -v2.y           +          v2.x           + gamepad1.right_stick_x;
-                double frontLeftPower  =          -v2.y           -          v2.x           - gamepad1.right_stick_x;
-                double backRightPower  =          -v2.y           -          v2.x           + gamepad1.right_stick_x;
-                double backLeftPower   =          -v2.y           +          v2.x           - gamepad1.right_stick_x;
-                // endregion
+            //                      [        straight       ] [         lateral       ] [         turn         ]
+            double frontRightPower = -v2.y + v2.x + gamepad1.right_stick_x;
+            double frontLeftPower = -v2.y - v2.x - gamepad1.right_stick_x;
+            double backRightPower = -v2.y - v2.x + gamepad1.right_stick_x;
+            double backLeftPower = -v2.y + v2.x - gamepad1.right_stick_x;
+            // endregion
 
-                // region NORMALIZE MOTOR POWER
+            // region NORMALIZE MOTOR POWER
 
-                // finds the highest absolute value of the non normalized motor powers
-                double highestAbsoluteNum = Math.max(Math.max(Math.abs(frontRightPower), Math.abs(frontLeftPower)), Math.max(Math.abs(backRightPower), Math.abs(backLeftPower)));
+            // finds the highest absolute value of the non normalized motor powers
+            double highestAbsoluteNum = Math.max(Math.max(Math.abs(frontRightPower), Math.abs(frontLeftPower)), Math.max(Math.abs(backRightPower), Math.abs(backLeftPower)));
 
-                // if the motors aren't capable of the power requirement
-                if(highestAbsoluteNum > 1) {
-                    // normalize the motor powers to be between -1 to 1
-                    frontRightPower /= highestAbsoluteNum;
-                    frontLeftPower  /= highestAbsoluteNum;
-                    backRightPower  /= highestAbsoluteNum;
-                    backLeftPower   /= highestAbsoluteNum;
-                }
-                // endregion
+            // if the motors aren't capable of the power requirement
+            if (highestAbsoluteNum > 1) {
+                // normalize the motor powers to be between -1 to 1
+                frontRightPower /= highestAbsoluteNum;
+                frontLeftPower /= highestAbsoluteNum;
+                backRightPower /= highestAbsoluteNum;
+                backLeftPower /= highestAbsoluteNum;
+            }
+            // endregion
 
-                // region SET MOTOR POWER
-                frontRight.setPower(frontRightPower);
-                frontLeft .setPower(frontLeftPower );
-                backRight .setPower(backRightPower );
-                backLeft  .setPower(backLeftPower  );
-                // endregion
+            // region SET MOTOR POWER
+            frontRight.setPower(frontRightPower);
+            frontLeft.setPower(frontLeftPower);
+            backRight.setPower(backRightPower);
+            backLeft.setPower(backLeftPower);
+            // endregion
 
-      }
-   }
+        }
+    }
 }
